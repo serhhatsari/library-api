2da8c2bfaede725220236bc25455cd626b160288
Serhat SARI <serhhatsari@gmail.com>
2023-04-15 22:34:53 +0300

    feat: implement author handler

--- handler/author_handler.go
+++ handler/author_handler.go
@@ -1,11 +1,14 @@
 package handler
 
 import (
-	"encoding/json"
-	"fmt"
+	"github.com/go-chi/chi/v5"
 	jsoniter "github.com/json-iterator/go"
+	"github.com/serhhatsari/library-api/model"
+	log "github.com/serhhatsari/library-api/pkg/logger"
 	"github.com/serhhatsari/library-api/repository"
+	"io"
 	"net/http"
+	"strconv"
 )
 
 func GetAuthors(repo *repository.Repository) http.HandlerFunc {
@@ -13,71 +16,166 @@ func GetAuthors(repo *repository.Repository) http.HandlerFunc {
 
 		authors, err := repo.GetAuthors()
 		if err != nil {
-			fmt.Println(err)
+			log.Logger().Error(err.Error())
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
-		w.WriteHeader(http.StatusOK)
+		if len(authors) == 0 {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
 		w.Header().Set("Content-Type", "application/json")
 
 		res, err := jsoniter.Marshal(authors)
 		if err != nil {
-			fmt.Println(err)
+			log.Logger().Error(err.Error())
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
 		_, err = w.Write(res)
 		if err != nil {
-			fmt.Println(err)
+			log.Logger().Error(err.Error())
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-
+		w.WriteHeader(http.StatusOK)
 	}
 }
 
 func CreateAuthor(repo *repository.Repository) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		w.Header().Set("Content-Type", "application/json")
-		err := json.NewEncoder(w).Encode([]byte{})
+		var author model.Author
+
+		body, err := io.ReadAll(r.Body)
 		if err != nil {
+			log.Logger().Error(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+
+		err = jsoniter.Unmarshal(body, &author)
+		if err != nil {
+			log.Logger().Error(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		err = repo.CreateAuthor(author)
+		if err != nil {
+			log.Logger().Error(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusCreated)
 	}
 }
 
 func GetAuthor(repo *repository.Repository) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-
-		w.Header().Set("Content-Type", "application/json")
-		err := json.NewEncoder(w).Encode([]byte{})
+		idStr := chi.URLParam(r, "id")
+		id, err := strconv.Atoi(idStr)
 		if err != nil {
+			log.Logger().Error(err.Error())
+			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
+
+		author, err := repo.GetAuthor(id)
+		if err != nil {
+			log.Logger().Error(err.Error())
+			if err.Error() == "no rows in result set" {
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+
+		res, err := jsoniter.Marshal(author)
+		if err != nil {
+			log.Logger().Error(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		_, err = w.Write(res)
+		if err != nil {
+			log.Logger().Error(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
 	}
 }
 
 func UpdateAuthor(repo *repository.Repository) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		w.Header().Set("Content-Type", "application/json")
-		err := json.NewEncoder(w).Encode([]byte{})
+		var author model.Author
+
+		idStr := chi.URLParam(r, "id")
+		id, err := strconv.Atoi(idStr)
 		if err != nil {
+			log.Logger().Error(err.Error())
+			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Logger().Error(err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		err = jsoniter.Unmarshal(body, &author)
+		if err != nil {
+			log.Logger().Error(err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		err = repo.UpdateAuthor(id, author)
+		if err != nil {
+			log.Logger().Error(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
 	}
 }
 
 func DeleteAuthor(repo *repository.Repository) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		strId := chi.URLParam(r, "id")
+		id, err := strconv.Atoi(strId)
 
-		w.Header().Set("Content-Type", "application/json")
-		err := json.NewEncoder(w).Encode([]byte{})
 		if err != nil {
+			log.Logger().Error(err.Error())
+			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
+
+		err = repo.DeleteAuthor(id)
+		if err != nil {
+			log.Logger().Error(err.Error())
+			if err.Error() == "author not found" {
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
 	}
 }
--- repository/author_repository.go
+++ repository/author_repository.go
@@ -2,7 +2,7 @@ package repository
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"github.com/serhhatsari/library-api/model"
 	log "github.com/serhhatsari/library-api/pkg/logger"
 )
@@ -20,11 +20,58 @@ func (repo *Repository) GetAuthors() ([]model.Author, error) {
 		var author model.Author
 		err = rows.Scan(&author.ID, &author.Name, &author.Biography, &author.BirthDate)
 		if err != nil {
-			fmt.Println(err)
+			log.Logger().Error(err.Error())
 			return nil, err
 		}
 		authors = append(authors, author)
 	}
 
 	return authors, nil
+}
+
+func (repo *Repository) CreateAuthor(author model.Author) error {
+	_, err := repo.conn.Exec(context.Background(), "INSERT INTO authors (name, biography, birth_date) VALUES ($1, $2, $3)", author.Name, author.Biography, author.BirthDate)
+	if err != nil {
+		log.Logger().Error(err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func (repo *Repository) GetAuthor(id int) (model.Author, error) {
+	var author model.Author
+
+	row := repo.conn.QueryRow(context.Background(), "SELECT * FROM authors WHERE id = $1", id)
+	err := row.Scan(&author.ID, &author.Name, &author.Biography, &author.BirthDate)
+	if err != nil {
+		log.Logger().Error(err.Error())
+		return model.Author{}, err
+	}
+
+	return author, nil
+}
+
+func (repo *Repository) UpdateAuthor(id int, author model.Author) error {
+	_, err := repo.conn.Exec(context.Background(), "UPDATE authors SET name = $1, biography = $2, birth_date = $3 WHERE id = $4", author.Name, author.Biography, author.BirthDate, id)
+	if err != nil {
+		log.Logger().Error(err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func (repo *Repository) DeleteAuthor(id int) error {
+	tag, err := repo.conn.Exec(context.Background(), "DELETE FROM authors WHERE id = $1", id)
+	if err != nil {
+		log.Logger().Error(err.Error())
+		return err
+	}
+
+	if tag.RowsAffected() == 0 {
+		return errors.New("author not found")
+	}
+
+	return nil
 }
